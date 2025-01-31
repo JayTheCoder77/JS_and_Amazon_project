a85405da41676a96aa617a821ad42924b96e8b12
JayTheCoder77 <jayantiyer90@gmail.com>
2025-01-31 14:29:30 +0530

    cart button made interactive

--- 13-amazon/scripts/amazon.js
+++ 13-amazon/scripts/amazon.js
@@ -51,7 +51,7 @@ products.forEach((product) => {
         </div>
     `;
 });
-console.log(productsHtml);
+// console.log(productsHtml);
 
 document.querySelector('.js-products-grid')
 .innerHTML = productsHtml;
@@ -75,6 +75,13 @@ document.querySelectorAll('.js-add-to-cart')
             quantity : 1
         });
     }
-        console.log(cart);
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
 });
+
+

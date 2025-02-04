9526b6643d468214eb183b153988b6ddf4540ae0
JayTheCoder77 <jayantiyer90@gmail.com>
2025-02-04 22:54:43 +0530

    4 feb 2025

--- 13-amazon/data/cart.js
+++ 13-amazon/data/cart.js
@@ -6,14 +6,17 @@ cart = [
     {
         productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         quantity:2,
+        deliveryOptionId : '1'
     },
     {
         productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity:1
+        quantity:1,
+        deliveryOptionId : '2'
     },
     {
         productId:'83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-        quantity:2
+        quantity:2,
+        deliveryOptionId : '3'
     }
 ];
 }
@@ -34,7 +37,8 @@ export function addToCart(productId){
     else{
     cart.push({
         productId : productId,
-        quantity : 1
+        quantity : 1,
+        deliveryOptionId :'1',
     });
 }
     saveToStorage();
@@ -53,3 +57,14 @@ export function removeFromCart(productId){
     cart = newCart;
     saveToStorage();
 };
+
+export function updateDeliveryOption(productId,deliveryOptionId){
+    let matchingItem;
+    cart.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+            matchingItem = cartItem;
+        }
+    });
+    matchingItem.deliveryOptionId = deliveryOptionId;
+    saveToStorage();
+}
